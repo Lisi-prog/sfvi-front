7e73a7e4775a59a61ae738a127c18ecd18443ba5
Lisi-prog <80710956+Lisi-prog@users.noreply.github.com>
2024-10-11 17:33:05 -0300

    idontknow

--- src/components/InputSelectProgram.tsx
+++ src/components/InputSelectProgram.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios, {isCancel, AxiosError, AxiosResponse} from 'axios';
 
 const FileInput: React.FC = () => {
   const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -7,6 +8,27 @@ const FileInput: React.FC = () => {
         console.log('entra aqui')
       const filePath = await window.electron.getFilePath(file);
       console.log('Ruta del archivo seleccionado:', filePath);
+
+      const datos = [
+        {
+          "ruta": filePath
+        }
+      ]
+
+      axios.post(`${import.meta.env.VITE_HTTP_URL}:${import.meta.env.VITE_HTTP_PORT}/ruta`, datos, 
+          {
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json;charset=UTF-8',
+            }
+          })
+          .then(response => {
+              console.log('La respuesta del servidor es: '+response.data)
+          })
+          .catch(function(error) {
+              console.log(`Error: ${error}`);
+          });
+
     }
   };
 
