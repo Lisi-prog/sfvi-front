f9a2be2f1bc1224feac20895dbc32f6646f2dc74
JDCaruza <j.d.caruza@gmail.com>
2024-10-25 11:58:45 -0300

    para lisi con amors

--- src/components/InputSelectProgram.tsx
+++ src/components/InputSelectProgram.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-
-const FileInput: React.FC = () => {
+import React, { useState } from 'react';
+export interface FilePath {
+  getFilePath: (filePath: string) => void;
+}
+const FileInput: React.FC<FilePath> = (props) => {
+  const [selectedFilePath, setSelectedFilePath] = useState<string | null>(null);
   const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
-        console.log('entra aqui')
+      //console.log('entra aqui')
       const filePath = await window.electron.getFilePath(file);
-      console.log('Ruta del archivo seleccionado:', filePath);
+      console.log('Ruta desde el hijo:', filePath);
+      setSelectedFilePath(filePath);
+      props.getFilePath(filePath);
     }
   };
 
--- src/components/MainDrawer.tsx
+++ src/components/MainDrawer.tsx
@@ -1,11 +1,16 @@
 import { Box, Button, Collapse, Dialog, DialogActions, DialogContent, DialogTitle, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Modal, styled } from "@mui/material";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { AvTimer, MonitorHeart, PhotoCamera, RestartAlt, Settings, Tv } from "@mui/icons-material";
 import logoUrl from '../assets/zero-logo.png'
 import { start } from "repl";
 import { GeneralConfiguration } from "../views/GeneralConfiguration";
 import { CameraConfiguration } from "../views/CameraConfiguration";
-import InputSelectProgram from "./InputSelectProgram";
+import InputSelectProgram, { FilePath } from "./InputSelectProgram";
+import useWebSocket, { ReadyState } from 'react-use-websocket';
+
+const mensaje = {
+  codigo: 'dameimagen'
+}
 
 declare global {
   interface Window {
@@ -40,17 +45,32 @@ async function abrirSelectorDeArchivos() {
 
 
 export default function MainDrawer() {
+  const [socketUrl, setSocketUrl] = useState(`${import.meta.env.VITE_URL_WEBSOCKET}:${import.meta.env.VITE_PUERTO_WEBSOCKET}`);
+  const { sendMessage, lastMessage, readyState, getWebSocket} = useWebSocket(socketUrl);
   const [openGeneralConfig, setOpenGeneralConfig] = React.useState(false);
   const handleOpenGeneralConfig = () => setOpenGeneralConfig(true);
-  const handleCloseGeneralConfig = () => setOpenGeneralConfig(false);
+  const handleCloseGeneralConfig = () => {
+    setOpenGeneralConfig(false);
+    sendMessage('ping')
+  }
   const [openCameraConfig, setOpenCameraConfig] = React.useState(false);
   const handleOpenCameraConfig = () => setOpenCameraConfig(true);
   const handleCloseCameraConfig = () => setOpenCameraConfig(false);
-
+  useEffect(() => {
+    sendMessage(JSON.stringify(mensaje))
+  }, [])
+  useEffect(() => {
+    console.log(lastMessage?.data)
+  }, [lastMessage])
+  
   const onOpenFileClick = () => {
     var x = document.getElementById("file-opener")
     x?.click()
+    sendMessage('Tocamos el boton Select Program')
   };
+  const dameFilePath = (filePath: string) => {
+    console.log(`Ruta desde el padre: ${filePath}`)
+  }
 
   const DrawerList = (
     <Box sx={{ width: '15vw' }} role="presentation">
@@ -129,8 +149,8 @@ export default function MainDrawer() {
           </ListItemIcon>
           <ListItemText primary="Select Program" />
         </ListItemButton>
-          <InputSelectProgram/>
-        <Divider></Divider>
+          <InputSelectProgram getFilePath={dameFilePath}/>
+        <Divider></Divider> 
         <ListItemButton  href='/Counters'>
           <ListItemIcon>
             <AvTimer />
